fb52d45fd0aa51b03501279f5f654cd5c0527a03
progtramder <wolf_wml@163.com>
2017-08-09 16:26:29 +0800

    Update test.go

--- test/test.go
+++ test/test.go
@@ -1,1 +1,42 @@
 
+//
+// Copyright 2017 The Alpha-firm. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+package main
+
+import (
+	"fmt"
+	"github.com/progtramder/webproxy"
+)
+
+type TestSniffer struct {}
+
+func (TestSniffer) BeforeRequest(s *webproxy.Session) {
+	fmt.Println(s.GetMethod(), s.GetRequestURL())
+	for k, v := range s.GetRequestHead() {
+		if v != nil {
+			fmt.Println(k, v)
+		}
+	}
+
+	fmt.Println("")
+}
+
+func (TestSniffer) BeforeResponse(s *webproxy.Session) {
+	fmt.Println(s.GetResponseProto(), s.GetStatus())
+	for k, v := range s.GetResponseHead() {
+		if v != nil {
+			fmt.Println(k, v)
+		}
+	}
+
+	fmt.Println("")
+}
+
+func main() {
+	proxy := webproxy.NewProxy(9999, TestSniffer{})
+	proxy.Start()
+}
